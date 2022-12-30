bba4c8cc578bed677181a807587f3c622fd3aa04
Soomin Hyun <kellyhyun@utexas.edu>
2022-12-30 11:37:13 -0600

    toggle revised

--- js/main.js
+++ js/main.js
@@ -1,20 +1,22 @@
-function showWord(word) {
-  document.getElementById('word-' + word).style.display = 'block';
-}
-
-function hideWord(word) {
-  document.getElementById('word-' + word).style.display = 'none';
-}
-
-document.querySelectorAll('[data-word]').forEach(function(button) {
-  button.addEventListener('click', function() {
-    const word = this.dataset.word;
-    for (let i = 1; i <= 3; i++) {
-      if (i === word) {
-        showWord(i);
-      } else {
-        hideWord(i);
-      }
-    }
+$(document).ready(function() {
+  $('[data-toggle]').click(function() {
+    const target = $(this).data('toggle');
+    $('#' + target).toggle();
   });
+});
+
+function toggleVisibility(id) {
+  const element = document.getElementById(id);
+  const elements = document.getElementsByClassName(element.className);
+  for (let i = 0; i < elements.length; i++) {
+    if (elements[i] !== element) {
+      elements[i].style.display = "none";
+    }
+  }
+  element.style.display = element.style.display === "none" ? "block" : "none";
+}
+
+
+document.querySelector('[data-toggle]').addEventListener('click', function() {
+  toggleVisibility(this.dataset.toggle);
 });
